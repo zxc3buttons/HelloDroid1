1728e7987d5da42947a0b682ab81a9d1c1f4fd5f
olegt <oleg.tokarev.89@inbox.ru>
2021-02-16 16:03:00 +0300

    Initial commit

---
+++ app/src/main/java/com/example/calculator/HelloDroidActivity.java
@@ -0,0 +1,90 @@
+package com.example.calculator;
+
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import android.view.View;
+
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class HelloDroidActivity extends AppCompatActivity {
+
+    private TextView message;
+    private int counter = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        message = findViewById(R.id.clickCounter);
+        ImageView droid = findViewById(R.id.droidImage);
+
+        //Define and attach click listener
+        droid.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                tapDroid();
+            }
+        });
+    }
+    private void tapDroid() {
+        counter++;
+        String countAsText;
+        /*
+         * In real applications you should not write switch like the one below.
+         * Use resource of type "Quantity strings (plurals)" instead.
+         * See https://developer.android.com/guide/topics/resources/string-resource#Plurals
+         */
+        switch (counter) {
+            case 1:
+                countAsText = "once";
+                break;
+            case 2:
+                countAsText = "twice";
+                break;
+            default:
+                countAsText = String.format("%d times", counter);
+        }
+        message.setText(String.format("You touched the droid %s", countAsText));
+    }
+
+        /*Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+       FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });*/
+
+/*@Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }*/
+}
